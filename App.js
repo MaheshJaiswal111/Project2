59d2bb875129fe1779d09cf0fef4f0f80860a5f3
Mahesh Jaiswal <122383038+MaheshJaiswal111@users.noreply.github.com>
2023-05-15 23:19:07 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WordFrequencyAnalyzer from './WordFrequencyAnalyzer';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Word Frequency Analyzer</h1>
+      <WordFrequencyAnalyzer />
+    </div>
+  );
+};
+
+export default App;
